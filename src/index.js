805dfb944195dc6dafd1217fc74f831928f9bfa8
Marina Tarasevich <marysechka-2009@mail.ru>
2023-01-08 01:51:52 +0300

    docs: Add a new solution in Index file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = function reverse (n) {
    let num = n.toString();
-    return num.split("").reverse().join("");
+    let i = num.split("").reverse().join("");
+    return parseInt(i,10);
 }
